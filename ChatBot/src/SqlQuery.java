ded1f0ed4ae4612d05927e86d96e995414c754df
Mike Nowicki <mikenowicki022@outlook.com>
2014-01-25 16:59:05 -0800

    Added sample query
    Very simple connection/query classes. The complexity can be increased as
    the DB structure and parsed input forms become known.

--- ChatBot/src/SqlQuery.java
+++ ChatBot/src/SqlQuery.java
@@ -25,24 +25,37 @@ public class SqlQuery<E> {
 		
 	}
 	
-	public void userQuery(ArrayList<? extends Object> a) throws SQLException{
+	// Method takes user input in a generic array list to be used
+	// for an SQL query.
+	public String userQuery(ArrayList<? extends Object> a) throws SQLException{
 		
 		String query = "select AI_OUT from " + dbc.getDBName() +
 				".something where USER_IN = " + a.get(0);
+		
+		String AI_OUT = null;
+		
 		try {
 			stmt = dbc.getConn().createStatement();
 	        ResultSet rs = stmt.executeQuery(query);
-	        while (rs.next()) {
-	            String AI_OUT = rs.getString("AI_OUT");
-	        }
 	        
+	        // Create and execute a simple query. Selected data/columns
+	        // can be changed as the DB structure is known. 
+	        
+	        // If we want multiple responses these can be stored in an array
+	        // and maybe use an RNG to pick one from the array to return.
+	        while (rs.next()) {
+	            AI_OUT = rs.getString("AI_OUT");
+	        }
 	    } catch (SQLException e ) {
 	        e.printStackTrace();
+	        return null;
 	    } finally {
 	    	if (stmt != null){ 
 	        	stmt.close(); 
-	    	}
+	    	} 	
 	    }
+		
+		return AI_OUT;
 		
 	}
 	
