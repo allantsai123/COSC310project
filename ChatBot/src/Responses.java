4a55b408738703ea6cb2c5714c85f918c0e604a8
Mike <mikenowicki022@gmail.com>
2014-01-27 14:16:21 -0800

    Preparing dictionary
    Forming script, AI dictionary.

--- ChatBot/src/Responses.java
+++ ChatBot/src/Responses.java
@@ -7,6 +7,9 @@ public class Responses {
    public static List<String> farewells = Arrays.asList("Good <TimeOfDay>", "Bye", "Goodbye", "Farewell", "See ya", "See you later", "Take care", "Ciao");
    public static List<String> sorrybusys = Arrays.asList("I've moved on to helping someone else.", "Sorry, I'm busy now.", "I'm busy, try again later");
 
+   // Keep building lists
+   // Build a keywords class with similar structure
+   // Finish script
 
 
     public static String getRandomResponse(List<String> responses) {
