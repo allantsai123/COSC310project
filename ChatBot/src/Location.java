c833649b73f5e6f2239eead34380d56ef0535781
brett_d@shaw.ca <accounts@kpddoor.com>
2014-01-27 22:31:04 -0800

    add LocationFactory test

---
+++ ChatBot/src/Location.java
@@ -0,0 +1,11 @@
+
+public class Location {
+	public String origin = "kelowna, bc";
+	public String place;
+	public double tempInCelcius;
+	public String weatherDescription;
+	public double distanceFromOrigin;	
+	public void setOrigin(String origin){
+		this.origin = origin;
+	}
+}
